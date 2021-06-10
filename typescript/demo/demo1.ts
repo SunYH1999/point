f24d7668f3c072904415bf141678553725fbc4bc
SunYH1999 <sunyuhangvika@163.com>
2021-06-10 20:09:49 +0800

    20210610

---
+++ typescript/demo/demo1.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: sunyuhang 
+ * @Date: 2021-06-10 18:35:02 
+ * @Last Modified by: sunyuhang
+ * @Last Modified time: 2021-06-Th 07:52:13
+ */
+function demo() {
+    let web: string = "Hello World"
+    console.log(web)
+}
+
+demo()
---
+++ typescript/demo/demo2.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: sunyuhang 
+ * @Date: 2021-06-Th 08:01:24 
+ * @Last Modified by:   sunyuhang 
+ * @Last Modified time: 2021-06-Th 08:01:24 
+ */
+const count : number = 1
+//类型不可改变，继承number属性和方法
